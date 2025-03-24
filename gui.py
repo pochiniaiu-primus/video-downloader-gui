fb9dd24571a64a7cf35b57f13fa3a5ec1f36528c
Serhii Honchar <serhii.honchar.java@gmail.com>
2025-03-24 23:05:34 +0100

    Add download_audio function and refactor UI.

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
+import logging
+import os
 import tkinter as tk
 from tkinter import messagebox, filedialog
-import logging
-from pytubefix import YouTube
+
 from moviepy import *
 from pytube.cli import on_progress
+from pytubefix import YouTube
 
 logging.basicConfig(
     filename="video_downloader.log",
@@ -19,20 +21,21 @@ class GUI:
         self.root.title("Video Downloader")
         self.root.geometry("500x400")
         self.root.config(padx=10, pady=10)
+        self.download_directory = None
 
-        self.canvas = tk.Canvas(self.root, width=400, height=300)
+        self.canvas = tk.Canvas(self.root, width=400, height=360)
         self.canvas.pack()
 
         self.app_label = tk.Label(self.root, text="Video Downloader", fg="blue", font=("Arial", 30))
         self.canvas.create_window(200, 20, window=self.app_label)
 
-        self.url_label = tk.Label(self.root, text="Enter video URL", font=("Arial", 15))
+        self.url_label = tk.Label(self.root, text="Enter video URL:", font=("Arial", 15))
         self.canvas.create_window(200, 80, window=self.url_label)
 
         self.url_entry = tk.Entry(self.root, width=60)
         self.canvas.create_window(200, 100, window=self.url_entry)
 
-        self.path_label = tk.Label(self.root, text="Select path to download", font=("Arial", 15))
+        self.path_label = tk.Label(self.root, text="Select download directory", font=("Arial", 15))
         self.canvas.create_window(200, 150, window=self.path_label)
 
         self.path_button = tk.Button(
@@ -41,11 +44,23 @@ class GUI:
         )
         self.canvas.create_window(200, 180, window=self.path_button)
 
-        self.download_button = tk.Button(
-            self.root, text="Download", width=30, font=("Helvetica", 12, "bold"),
-            command=self.download, highlightthickness=0
+        self.download_video_button = tk.Button(
+            self.root, text="Download Video", width=30, font=("Helvetica", 12, "bold"),
+            command=self.download_video, highlightthickness=0
         )
-        self.canvas.create_window(200, 250, window=self.download_button)
+        self.canvas.create_window(200, 230, window=self.download_video_button)
+
+        self.download_audio_button = tk.Button(
+            self.root, text="Download audio from video", width=30, font=("Helvetica", 12, "bold"),
+            command=self.download_audio, highlightthickness=0
+        )
+        self.canvas.create_window(200, 280, window=self.download_audio_button)
+
+        self.exit_button = tk.Button(
+            self.root, text="Exit", width=30, font=("Helvetica", 12, "bold"),
+            command=self.root.quit, highlightthickness=0
+        )
+        self.canvas.create_window(200, 330, window=self.exit_button)
 
     def select_download_directory(self):
         file_path = filedialog.askdirectory(title="Select Download Directory")
@@ -54,11 +69,11 @@ class GUI:
             self.path_label.config(text=file_path)
         else:
             logging.info("No download directory was selected.")
-            self.path_label.config(text="Select path to download")
+            self.path_label.config(text="Select download directory")
 
-    def download(self):
+    def download_video(self):
         """
-        Download a YouTube video using the provided URL and save it to a user-selected directory.
+        Download a YouTube video using the provided URL and save it to the user-selected directory.
         """
 
         video_url = self.url_entry.get()
@@ -70,7 +85,7 @@ class GUI:
             messagebox.showwarning("Warning", "No video link provided.")
             return
 
-        if not download_directory or download_directory == "Select path to download":
+        if not download_directory or download_directory == "Select download directory":
             logging.warning("No valid download directory provided.")
             messagebox.showwarning("Warning", "Please select a valid download directory.")
             return
@@ -92,3 +107,42 @@ class GUI:
         except Exception as e:
             logging.error(f"Error downloading video: {e}", exc_info=True)
             messagebox.showerror("Error", f"Error downloading video: {e}")
+
+    def download_audio(self):
+
+        video_url = self.url_entry.get().strip()
+        download_directory = self.path_label.cget("text").strip()
+
+        try:
+            logging.info(f"Starting audio download for video: {video_url} to directory: {download_directory}")
+            yt = YouTube(video_url, on_progress_callback=on_progress)
+
+            # Filter for streams that are audio only
+            audio_stream = yt.streams.filter(only_audio=True).first()
+
+            if audio_stream is None:
+                logging.error("No valid download directory provided.")
+                raise Exception("No audio stream available for this video.")
+
+            file_path = filedialog.asksaveasfilename(
+                defaultextension=".mp3",
+                filetypes=[("mp3 files", "*.mp3")],
+                title="Save Audio"
+            )
+            if not file_path:
+                logging.info("Audio save canceled by the user.")
+                return
+
+            # Extract the directory and filename from user-provided path.
+            out_directory, out_filename = os.path.split(file_path)
+
+            if not out_directory:
+                out_directory = download_directory
+
+            # Download the audio stream directly to the specified location.
+            audio_stream.download(output_path=out_directory, filename=out_filename)
+            logging.info(f"Audio downloaded successfully to: {os.path.join(out_directory, out_filename)}")
+            messagebox.showinfo("Success", "Audio downloaded successfully.")
+        except Exception as e:
+            logging.error(f"Error downloading audio: {e}", exc_info=True)
+            messagebox.showerror("Error", f"Error downloading audio: {e}")
